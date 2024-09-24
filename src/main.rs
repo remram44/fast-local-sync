5cd541f3547a718e351e6b3b599328b43d605b25
Remi Rampin <remi@rampin.org>
2024-09-24 19:29:53 -0400

    Move stats report to stats.rs

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,6 @@ use std::env::args_os;
 use std::ffi::OsString;
 use std::path::PathBuf;
 use std::process::exit;
-use std::sync::Arc;
-use std::thread;
-use std::time::Duration;
 
 fn parse_num_option(opt: Option<OsString>, flag: &'static str) -> usize {
     let opt = match opt {
@@ -81,7 +78,7 @@ fn main() {
     };
 
     // Initialize statistics
-    let stats = Arc::new(stats::Stats::new(entries, size));
+    let stats = stats::Stats::new(entries, size);
 
     // Create worker pools
     let file_copy_pool = file_copier::FileCopyPool::new(
@@ -100,38 +97,6 @@ fn main() {
 
     // Enqueue work
     dir_scan_pool.add("/".into());
-
-    // Print stats regularly
-    {
-        let stats = stats.clone();
-        thread::spawn(move || {
-            let stats = &*stats;
-
-            let mut i = 0;
-
-            loop {
-                thread::sleep(Duration::from_secs(10));
-
-                i += 1;
-                if i >= 30 {
-                    i = 1;
-                    eprintln!(
-                        "SCANNED   \
-                         SKIPPED   \
-                         QUEUED    \
-                         COPIED"
-                    );
-                }
-                eprintln!(
-                    "{:>9} {:>9} {:>9} {:>9}",
-                    stats.scanned_entries(),
-                    stats.skipped_entries(),
-                    stats.queued_copy_entries(),
-                    stats.copied_entries(),
-                )
-            }
-        });
-    }
 
     // Wait until done
     dir_scan_pool.join();
--- src/stats.rs
+++ src/stats.rs
@@ -1,4 +1,7 @@
+use std::sync::Arc;
 use std::sync::atomic::{AtomicUsize, Ordering};
+use std::thread;
+use std::time::Duration;
 
 pub struct Stats {
     total_entries: Option<usize>,
@@ -10,14 +13,50 @@ pub struct Stats {
 }
 
 impl Stats {
-    pub fn new(total_entries: Option<usize>, total_size: Option<usize>) -> Stats {
-        Stats {
+    pub fn new(total_entries: Option<usize>, total_size: Option<usize>) -> Arc<Stats> {
+        let stats = Arc::new(Stats {
             total_entries,
             total_size,
             scanned_entries: AtomicUsize::new(0),
             skipped_entries: AtomicUsize::new(0),
             queued_copy_entries: AtomicUsize::new(0),
             copied_entries: AtomicUsize::new(0),
+        });
+
+        {
+            let stats = stats.clone();
+            thread::spawn(move || {
+                let stats = &*stats;
+                stats.print_thread();
+            });
+        }
+
+        stats
+    }
+
+    fn print_thread(&self) {
+        let mut i = 0;
+
+        loop {
+            thread::sleep(Duration::from_secs(10));
+
+            i += 1;
+            if i >= 30 {
+                i = 1;
+                eprintln!(
+                    "SCANNED   \
+                     SKIPPED   \
+                     QUEUED    \
+                     COPIED"
+                );
+            }
+            eprintln!(
+                "{:>9} {:>9} {:>9} {:>9}",
+                self.scanned_entries(),
+                self.skipped_entries(),
+                self.queued_copy_entries(),
+                self.copied_entries(),
+            )
         }
     }
 
