59dae933cb649e569ab74c6c02eee5a2780c3c97
Remi Rampin <remi@rampin.org>
2024-09-24 20:28:07 -0400

    Fix copying symlinks

--- src/copy.rs
+++ src/copy.rs
@@ -1,17 +1,19 @@
 use filetime::{FileTime, set_symlink_file_times};
 use std::io::ErrorKind;
-use std::fs::{copy, create_dir, set_permissions, symlink_metadata};
-use std::os::unix::fs::{MetadataExt, chown};
+use std::fs::{copy, create_dir, read_link, set_permissions, symlink_metadata};
+use std::os::unix::fs::{MetadataExt, lchown, symlink};
 use std::path::Path;
-use tracing::debug;
+use tracing::{debug, warn};
 
 fn copy_metadata(source: &Path, target: &Path) -> std::io::Result<()> {
     // Get metadata of source
     let metadata = symlink_metadata(source)?;
 
     // Copy attributes
-    chown(target, Some(metadata.uid()), Some(metadata.gid()))?;
-    set_permissions(target, metadata.permissions())?;
+    lchown(target, Some(metadata.uid()), Some(metadata.gid()))?;
+    if !metadata.is_symlink() {
+        set_permissions(target, metadata.permissions())?;
+    }
     let mtime = FileTime::from_last_modification_time(&metadata);
     set_symlink_file_times(target, mtime, mtime)?;
 
@@ -36,7 +38,16 @@ pub fn copy_directory(source: &Path, target: &Path) -> std::io::Result<()> {
 pub fn copy_file(source: &Path, target: &Path) -> std::io::Result<()> {
     debug!("copy_file {:?} {:?}", source, target);
 
-    copy(source, target)?;
+    let source_metadata = symlink_metadata(source)?;
+
+    if source_metadata.is_symlink() {
+        let link = read_link(source)?;
+        symlink(link, target)?;
+    } else if source_metadata.is_file() {
+        copy(source, target)?;
+    } else {
+        warn!("Don't know how to copy entry that's not a symlink or a file: {:?}", source);
+    }
 
     copy_metadata(source, target)
 }
